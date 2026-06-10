33bc902f1b1fbda686e8bec5021f7154b27b933f
Yasleidy Palacios <yasleidypalacios@gmail.com>
2026-06-10 00:43:18 -0500

    feat: agregar endpoint extraer al controller

--- app/controllers/etl_controller.py
+++ app/controllers/etl_controller.py
@@ -16,3 +16,15 @@ router = APIRouter(
 @router.get("/")
 def prueba():
     return {"mensaje": "API funcionando"}
+@router.post("/extraer", status_code=201)
+def extraer(request: ExtraccionRequest):
+    if request.cantidad <= 0:
+        return {"error": "La cantidad debe ser mayor que cero"}
+    cartas = obtener_cartas(request.cantidad)
+    insertadas = guardar_cartas_mongo(cartas)
+    return {
+        "mensaje": "Datos extraídos exitosamente",
+        "registros_guardados": insertadas,
+        "fuente": "Pokémon TCG API",
+        "status": 201
+    }
