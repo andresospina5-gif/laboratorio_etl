a0dc16f726e24a668a17625162c2dc5dbd73d053
danielchavarria-prog <daniel.chavarriaa@udea.edu.co>
2026-06-11 18:06:44 -0500

    agregar modulo analitica completo

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from app.controllers.etl_controller import router
+from app.controllers.etl_controller import router as etl_router
+from app.controllers.analitica_controller import router as analitica_router
 
 app = FastAPI(
     title="Laboratorio ETL"
 )
 
-app.include_router(router)
+app.include_router(etl_router)
+app.include_router(analitica_router)
